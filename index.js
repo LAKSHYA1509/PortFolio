052e3f564b56794f82d4534337db829346586155
LAKSHYA BHARDWAJ <123384759+LAKSHYA1509@users.noreply.github.com>
2024-07-18 19:30:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+Shery.mouseFollower();
+Shery.makeMagnet(".main-header" /* Element to target.*/, {
+  //Parameters are optional.
+});
+Shery.textAnimate(".heading-primary__main" /* Element to target.*/, {
+  //Parameters are optional. 
+  style: 2,
+  y: 8,
+  delay: 0.1,
+  duration: 3,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.01,
+});
+
+Shery.imageEffect(".holatiyo", {
+  style: 1, //Select Style
+  debug: true, // Debug Panel
+});
+  
